9d56f80569f0a64ee855117191b8b49cf574d4e8
yassineslim <46113776+yassineslim@users.noreply.github.com>
2022-01-16 14:55:10 +0100

    change

--- contracts/donnation.sol
+++ contracts/donnation.sol
@@ -14,8 +14,47 @@ contract donnation {
         bool status;
         address createur;
     }
-
-    function createvent(string memory titre, string memory description, uint objectif) public {
-        
+    struct HomeCard{
+        string titre;
+        uint id;
     }
+
+    event eventcreated(id);
+    event donnated(uint montant);
+    function createvent(string memory titre, string memory description, uint objectif) public {
+        uint idnevent = eventid;
+        address eventcreat = msg.sender;
+        donevent memory newdonevent = donevent(titre, description, objectif, 0, id_newdonevent, true, eventcreat);
+
+        donnations[id_newdonevent] = newdonevent;
+
+        eventid +=1;
+
+        emit eventcreated(id_newdonevent);
+    }
+
+    function donate(uint id_event) public payable {
+        donevent storage donnation = donnations[id_event];
+        uint montant = msg.valeur;
+        donnation.actuel = donnation.actuel + montant;
+
+        emit donated(montant);
+    }
+    function retirer(uint id_event) public{
+        address payable retirecompte = payable(msg.sender);
+        donevent storage donnation = donnations[id_event];
+
+        require(retirecompte == donnation.creator);
+        retirecompte.transfer(donnation.actuel);
+        donnation.status = false;
+    }
+    
+    function getdataHome() public view returns (HomeCard[] memory) {
+        HomeCard[] memory cards = new HomeCard[](eventid);
+        for (uint i = 0; i < eventid, i++){
+            HomeCard memory homeCard = HomeCard(donnation[i].title, i);
+            cards[i] = homeCard;
+        }
+        return cards;
+    } 
 }
